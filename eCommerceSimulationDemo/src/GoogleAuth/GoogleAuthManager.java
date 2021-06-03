177ab998d9ed39f96e43e654799f32be1cf59e48
GizemDuyguSonmez <83882284+GizemDuyguSonmez@users.noreply.github.com>
2021-06-03 19:15:21 +0300

    Update GoogleAuthManager.java

--- eCommerceSimulationDemo/src/GoogleAuth/GoogleAuthManager.java
+++ eCommerceSimulationDemo/src/GoogleAuth/GoogleAuthManager.java
@@ -3,10 +3,10 @@ package GoogleAuth;
 public class GoogleAuthManager {
 
 	public void register(String email,String password) {
-   	 System.out.println("Google ile kayýt alýndý :" + email);
+   	 System.out.println("Google ile kayit alindi :" + email);
    	 
     }
     public void login(String email,String password) {
-   	 System.out.println("Google ile giriþ yapýldý : " + email);
+   	 System.out.println("Google ile giris yapildi : " + email);
     }
 }
